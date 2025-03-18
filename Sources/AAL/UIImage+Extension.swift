6eadd1b12b20f0ed7f60cd721f21e5dd6d1666ba
AshishT30 <ashish.tyagi@bharatpe.com>
2025-03-18 10:38:51 +0530

    code updated

--- Sources/AAL/UIImage+Extension.swift
+++ Sources/AAL/UIImage+Extension.swift
@@ -10,10 +10,6 @@ import UIKit
 
 extension UIImage {
     static func loadImage(named imageName: String) -> UIImage? {
-        if #available(iOS 13.0, *) {
-              return UIImage(named: imageName, in: Bundle.module, with: nil)
-          } else {
-              return UIImage(named: imageName) // Fallback for iOS 12 and earlier
-          }
+        return UIImage(named: imageName) // Fallback for iOS 12 and earlier
     }
 }
